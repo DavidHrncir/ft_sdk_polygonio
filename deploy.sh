5114df735c269eef28788fe4713000e5e167a690
David Hrncir <david.hrncir@fivetran.com>
2025-05-19 10:43:36 -0500

    Updated config file.

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,24 @@
 
 # Get the config path env var. This is the path where the keys are located for the FT accounts.
 CONFIG_PATH="${FT_CONFIG_KEY_PATH}"
+CONFIG_FILE="ft_accounts_config.json"
 
 # Prompt for the Fivetran Account Name (default is current dbx hol acct)
 read -p "Enter your Fivetran Account Name [MDS_DATABRICKS_HOL]: " ACCOUNT_NAME
 ACCOUNT_NAME=${ACCOUNT_NAME:-"MDS_DATABRICKS_HOL"}
 
 # Read API key from ft_accounts_config.json based on account name
-API_KEY=$(jq -r ".fivetran.api_keys.$ACCOUNT_NAME" "$CONFIG_PATH/ft_accounts_config.json")
+API_KEY=$(jq -r ".fivetran.$ACCOUNT_NAME.api_key" "$CONFIG_PATH/$CONFIG_FILE")
 
 if [ "$API_KEY" == "null" ]; then
-    echo "Error: Account name not found in ft_accounts_config.json"
+    echo "Error: Account name not found in $CONFIG_PATH/$CONFIG_FILE"
     exit 1
 fi
 
 # Prompt for the Fivetran Destination Name (default is current dbx dest)
-read -p "Enter your Fivetran Destination Name [AZURE_DATABRICKS_UC_NATIVE_TS_DEMO]: " DESTINATION_NAME
-DESTINATION_NAME=${DESTINATION_NAME:-"AZURE_DATABRICKS_UC_NATIVE_TS_DEMO"}
+CONFIG_DEST_NAME=$(jq -r ".fivetran.$ACCOUNT_NAME.dest_name" "$CONFIG_PATH/$CONFIG_FILE")
+read -p "Enter your Fivetran Destination Name [$CONFIG_DEST_NAME]: " DESTINATION_NAME
+DESTINATION_NAME=${DESTINATION_NAME:-"$CONFIG_DEST_NAME"}
 
 # Prompt for the Fivetran Connector Name
 read -p "Enter a unique Fivetran Connector Name [default-connection]: " CONNECTION_NAME
